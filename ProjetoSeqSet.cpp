3d03322a26226203ab1fa9f816817651e8a36659
Gui Maia <guilhermemaia05@outlook.com>
2025-01-13 17:32:24 -0300

    Arquivo

--- ProjetoSeqSet.cpp
+++ ProjetoSeqSet.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-struct trade
+struct Trade
 {
     int time_ref;
     string account;
@@ -13,7 +14,40 @@ struct trade
     char status;
 };
 
+class InternationalTrade
+{
+    private:
+    Trade* registrosTrade;
+    int quantidadeTrades;
 
+    public:
+    InternationalTrade() : registrosTrade(NULL), quantidadeTrades(0){}
+
+    ~InternationalTrade() 
+    {
+        delete[] registrosTrade;
+        }
+
+    void carregarDeArquivo();
+    void adicionarTrade();
+    void removerTrade();
+    Trade* buscarTrade();
+};
+
+
+void InternationalTrade::carregarDeArquivo(){
+    ifstream arquivo ("international-trade-june-2022-quarter-csv.csv");
+
+    if (!arquivo.is_open()){
+        cout << "Erro ao abrir o arquivo!" << endl;
+        return;
+    }
+
+
+
+
+    arquivo.close();
+}
 
 
 
