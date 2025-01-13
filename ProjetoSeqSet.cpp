f2613db12b61827a3cc1512e4abdcee8f14dc7c5
Gui Maia <guilhermemaia05@outlook.com>
2025-01-13 16:21:26 -0300

    first commit

---
+++ ProjetoSeqSet.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+struct trade
+{
+    int time_ref;
+    string account;
+    int code;
+    string country_code;
+    string product_type;
+    float value;
+    char status;
+};
+
+
+
+
+
+
+
+
+
+int main(){
+
+
+
+
+
+    return 0;
+}
