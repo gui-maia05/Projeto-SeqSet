81bc6e4a2b363ba6df3c42460f5a84acec259d28
RczLuizao0 <luizphelipe99875@gmail.com>
2025-01-18 18:58:19 -0300

    sequence set de muleta

--- projetoSeqSet.cpp
+++ projetoSeqSet.cpp
@@ -1,62 +1,232 @@
 #include <iostream>
-#include <string>
+#include <fstream>
 
 using namespace std;
 
+const int Max = 100; //Tamanho máximo de registros por bloco
+
 struct RegistroTrade {
     int time_ref;
-    string account;
-    int code;
-    string country_code;
-    string product_type;
+    char account[30];
+    char code[10];
+    char country_code[5];
+    char product_type[30];
     double value;
     char status;
-};
 
-struct Noh {
-    RegistroTrade dado;
-    Noh* proximo;
+    // Comparador para ordenação por time_ref
+    bool operator<(const RegistroTrade &outro){
+        return time_ref < outro.time_ref;
+    }
+
 };
 
 class InternationalTrade {
+private:
+    int qtdTrades = 0;
+
+public:
+    InternationalTrade() {
+        qtdTrades = 0;
+    }
+
+    ~InternationalTrade() {}
+
+    void leituraArquivoCSV();
+    void transformarEmBinario(RegistroTrade* dados, int qtdTrades);
+    void leituraArquivoBinario();   
+};
+
+void InternationalTrade::leituraArquivoCSV() {
+    string linha;
+    char lixo;
+    int i = 0;
+
+    ifstream arquivoCSV("international-trade-june-2022-quarter-csv.csv");
+    if (!arquivoCSV) {
+        cout << "Erro ao abrir o arquivo CSV!" << endl;
+        return;
+    }
+
+    getline(arquivoCSV, linha); // Pular o cabeçalho
+
+    while (getline(arquivoCSV, linha)) {
+        qtdTrades++;
+    }
+
+    arquivoCSV.clear();
+    arquivoCSV.seekg(0, ios::beg);
+    getline(arquivoCSV, linha);
+    
+    RegistroTrade* dados = new RegistroTrade[qtdTrades];
+    
+    while (arquivoCSV >> dados[i].time_ref) {
+        arquivoCSV >> lixo; // Pular a vírgula
+        arquivoCSV.getline(dados[i].account, 50, ',');
+        arquivoCSV.getline(dados[i].code, 50, ',');
+        arquivoCSV.getline(dados[i].country_code, 50, ',');
+        arquivoCSV.getline(dados[i].product_type, 50, ',');
+        arquivoCSV >> dados[i].value;
+        if (arquivoCSV.fail()) {
+            dados[i].value = 0.0; // Se falhou, atribui 0.0
+            arquivoCSV.clear(); // Limpar o estado de falha
+            arquivoCSV >> lixo; 
+        } else {
+            arquivoCSV >> lixo; 
+        }
+        arquivoCSV.getline(dados[i].status, 10);
+        arquivoCSV.ignore();
+
+        i++;
+    }
+
+    arquivoCSV.close();
+
+    transformarEmBinario(dados, qtdTrades);
+
+    cout << "Arquivo binario criado com sucesso!" << endl;
+
+    delete[] dados;
+}
+
+void InternationalTrade::transformarEmBinario(RegistroTrade* dados, int qtdTrades) {
+    ofstream arquivoBinario("international-trade-june-2022-quarter-bin.bin", ios::binary);
+    arquivoBinario.write((char*)dados, sizeof(RegistroTrade) * qtdTrades);
+    arquivoBinario.close();
+}
+
+void InternationalTrade::leituraArquivoBinario() {
+    ifstream arquivoBinario("international-trade-june-2022-quarter-bin.bin", ios::binary);
+    if (!arquivoBinario) {
+        cout << "Erro ao abrir o arquivo binário!" << endl;
+        return;
+    }
+
+    arquivoBinario.seekg(0, ios::end);
+    streamsize tamanhoArquivo = arquivoBinario.tellg();
+    arquivoBinario.seekg(0, ios::beg);
+
+    qtdTrades = tamanhoArquivo / sizeof(RegistroTrade);
+
+    RegistroTrade* dados = new RegistroTrade[qtdTrades];
+    
+    arquivoBinario.read((char*)dados, tamanhoArquivo);
+    
+    arquivoBinario.close();
+
+    delete[] dados;
+}
+
+struct Bloco {
+    RegistroTrade registros[Max];
+    int numRegistros;
+    Bloco *proximo;
+
+    Bloco() {
+        numRegistros = 0;
+        proximo = nullptr;
+    }
+};
+
+struct Header {
+    int totalRegistros; // Número total de registros no Sequence Set
+    int totalBlocos;    // Número de blocos na estrutura
+    Bloco *primeiroBloco; // Referência ao primeiro bloco
+    Bloco *ultimoBloco;   // Referência ao último bloco
+};
+
+class SequenceSet {
     private:
-        Noh* head;
+        Bloco *head;
+
+        Bloco *EncontrarBloco(int chave) {
+            Bloco *atual = head;
+            while (atual != nullptr){
+                if (atual->numRegistros > 0 and chave <= atual->registros[atual->numRegistros - 1].time_ref) {
+                    return atual;
+                }
+                atual = atual->proximo;
+            }
+            return nullptr;
+        }
 
     public:
         //construtor
-        InternationalTrade(){
+        SequenceSet() {
             head = nullptr;
         }
         //destrutor
-        ~InternationalTrade() {
-            Noh* atual = head;
-            while (atual != nullptr) {
-                Noh* temp = atual;
-                atual = atual->proximo;
+        ~SequenceSet() {
+            while (head != nullptr) {
+                Bloco *temp = head;
+                head = head->proximo;
                 delete temp;
         }
     }
 
-    // Método para verificar se a lista está vazia
-    bool Vazia() const {
-        return head == nullptr;
+    // Inserir um registro
+    void Inserir(const RegistroTrade &registro) {
+        if (head == nullptr) {
+            head = new Bloco();
+        }
+
+        Bloco *bloco = EncontrarBloco(registro.time_ref);
+
+        if (bloco == nullptr) {
+            bloco = new Bloco();
+            bloco->proximo = head;
+            head = bloco;
+        }
+
+        if (bloco->numRegistros < Max) {
+            bloco->registros[bloco->numRegistros++] = registro;
+            sort(bloco->registros, bloco->registros + bloco->numRegistros);
+        } else {
+            // Overflow - criar novo bloco
+            Bloco *novoBloco = new Bloco();
+            for (int i = Max / 2; i < Max; ++i) {
+                novoBloco->registros[novoBloco->numRegistros++] = bloco->registros[i];
+            }
+            bloco->numRegistros = Max / 2;
+
+            novoBloco->proximo = bloco->proximo;
+            bloco->proximo = novoBloco;
+
+            if (registro.time_ref <= bloco->registros[bloco->numRegistros - 1].time_ref) {
+                Inserir(registro);
+            } else {
+                Inserir(registro);
+            }
+        }
     }
 
-    // Método para inserir um elemento no início da lista
-    void InserirInicio(const RegistroTrade& registro) {
-        Noh* novoNoh = new Noh{registro, head};
-        head = novoNoh;
-    }
-
-    // Método para remover o primeiro elemento da lista
-    void RemoverInicio() {
-        if (Vazia()) {
-            cout << "A lista está vazia.\n";
+    // Remover um registro
+    void Remover(int chave) {
+        Bloco *bloco = EncontrarBloco(chave);
+        if (bloco == nullptr) {
+            cout << "Registro não encontrado.\n";
             return;
         }
-        Noh* temp = head;
-        head = head->proximo;
-        delete temp;
+
+        auto it = find_if(bloco->registros, bloco->registros + bloco->numRegistros, [&](const RegistroTrade& r) {
+            return r.time_ref == chave;
+        });
+
+        if (it != bloco->registros + bloco->numRegistros) {
+            for (int i = distance(bloco->registros, it); i < bloco->numRegistros - 1; ++i) {
+                bloco->registros[i] = bloco->registros[i + 1];
+            }
+            bloco->numRegistros--;
+        }
+
+        if (bloco->numRegistros == 0 && head != bloco) {
+            Bloco* anterior = head;
+            while (anterior->proximo != bloco) {
+                anterior = anterior->proximo;
+            }
+            anterior->proximo = bloco->proximo;
+            delete bloco;
+        }
     }
 
     // Método para exibir os elementos da lista
@@ -72,7 +242,10 @@ class InternationalTrade {
         }
         cout << "NULL\n";
     }*/
+   
 };
+
+
 
 void Interface(){
     bool encontrado = true;
@@ -115,9 +288,10 @@ void Interface(){
         }*/
     }
 }
-// Função principal para testar a lista
+
+// Função principal
 int main() {
-    InternationalTrade lista;
+
     Interface();
 
     return 0;
