0c056101e9ef001d173db511cd9ea97adbe3da52
Gui Maia <guilhermemaia05@outlook.com>
2025-01-25 20:34:14 -0300

    Leitura em blocos no arquivo funcionando

--- ProjetoSeqSet.cpp
+++ ProjetoSeqSet.cpp
@@ -1,133 +1,312 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <sstream>
+#include <cstring>
+#include <iomanip>
 
 using namespace std;
+
+const int tamBloco = 50;
 
 struct RegistroTrade {
     int time_ref;
     char account[50];
-    char code[50];
-    char country_code[50];
-    char product_type[50];
-    float value;
-    char status[10];
-};
+    char code[10];
+    char country_code[5];
+    char product_type[30];
+    double value;
+    char status[5];
 
-class InternationalTrade {
-private:
-    int quantTrades = 0;
-
-public:
-    InternationalTrade() {
-        quantTrades = 0;
+    void display() const {
+        // Exibe os dados de um único registro
+        cout << left << setw(10) << time_ref; 
+        cout << setw(13) << account;
+        cout << setw(10) << code;
+        cout << setw(8) << country_code;
+        cout << setw(10) << product_type;
+        cout << setw(15) << fixed << setprecision(2) << value;
+        cout << setw(10) << status << endl;
     }
 
-    ~InternationalTrade() {}
-
-    void leituraArquivoCSV();
-    void transformarEmBinario(RegistroTrade* dados, int quantTrades);
-    void leituraArquivoBinario();   
 };
 
-void InternationalTrade::leituraArquivoCSV() {
-    string linha;
-    char lixo;
-    int i = 0;
+struct Bloco {
+    RegistroTrade registros[tamBloco];
+    int numRegistros;
+    Bloco *proximo;
 
-    ifstream arquivoCSV("international-trade-june-2022-quarter-csv.csv");
+    Bloco() {
+        numRegistros = 0;
+        proximo = nullptr;
+    }
+};
+
+class SequenceSet {
+    private:
+        Bloco *head;
+
+    public:
+
+        // Construtor
+        SequenceSet() {
+            head = nullptr;
+        }
+
+        // Destrutor
+        ~SequenceSet() {
+            Bloco *atual = head;
+            while (atual) {
+                Bloco *temp = atual;
+                atual = atual->proximo;
+                delete temp;
+            }
+        }
+
+        void inserirRegistro(const RegistroTrade &registro) {
+            if (!head) {
+                head = new Bloco();
+            }
+
+            Bloco *atual = head;
+
+            while (true) {
+                if (atual->numRegistros < tamBloco) {
+                    atual->registros[atual->numRegistros++] = registro;
+                    break;
+                }
+
+                if (!atual->proximo) {
+                    atual->proximo = new Bloco();
+                }
+                atual = atual->proximo;     
+            }
+        }
+
+        void MostrarDados() const {
+            Bloco* atual = head;
+            
+            // Cabeçalho da tabela
+            cout << left << setw(10) << "Time Ref";
+            cout << setw(13) << "Account";
+            cout << setw(10) << "Code";
+            cout << setw(8) << "Country Code";
+            cout << setw(10) << "Product Type";
+            cout << setw(15) << "Value";
+            cout << setw(10) << "Status" << endl;
+            cout << string(95, '-') << endl;
+
+            // Percorrer todos os blocos e registros
+            while (atual) {
+                for (int i = 0; i < atual->numRegistros; i++) {
+                    const RegistroTrade& record = atual->registros[i];
+                    cout << left << setw(10) << record.time_ref;
+                    cout << setw(13) << record.account;
+                    cout << setw(10) << record.code;
+                    cout << setw(8) << record.country_code;
+                    cout << setw(10) << record.product_type;
+                    cout << setw(15) << fixed << setprecision(2) << record.value;
+                    cout << setw(10) << record.status << endl;
+                }
+                atual = atual->proximo;
+            }
+        }
+
+        void exibirTodos() const {
+            Bloco* atual = head;
+            int numeroBloco = 1;
+
+            while (atual) {
+                cout << "\nBLOCO " << numeroBloco << endl << endl;
+
+                // Cabeçalho da tabela para cada bloco
+                cout << left << setw(10) << "TimeRef";
+                cout << setw(11) << "Account";
+                cout << setw(8) << "Code";
+                cout << setw(13) << "CountryCode";
+                cout << setw(13) << "ProductType";
+                cout << setw(8) << "Value";
+                cout << setw(10) << "Status" << endl;
+
+                cout << string(95, '-') << endl;
+
+                for (int i = 0; i < atual->numRegistros; i++) {
+                    atual->registros[i].display();
+                }
+
+                cout << string(95, '-') << endl;
+
+                atual = atual->proximo;
+                numeroBloco++;
+            }
+        }
+
+        int contarBlocos() const{
+            int count = 0;
+            Bloco *atual = head;
+            while (atual) {
+                count++;
+                atual = atual->proximo;
+            }
+            return count;
+        }
+
+        void salvarEmArquivo(const string &nomeArquivo) const {
+            ofstream arquivo(nomeArquivo, ios::binary);
+            if (!arquivo) {
+                cout << "Erro ao abrir o arquivo para salvar: " << nomeArquivo << endl;
+                return;
+            }
+
+            Bloco *atual = head;
+            while (atual) {
+                arquivo.write(reinterpret_cast<const char*>(&atual->numRegistros), sizeof(atual->numRegistros));
+                arquivo.write(reinterpret_cast<const char*>(atual->registros), sizeof(RegistroTrade) * atual->numRegistros);
+                atual = atual->proximo;
+            }
+
+            arquivo.close();
+        }
+
+        void carregarDeArquivo(const string &nomeArquivo) {
+            ifstream arquivo(nomeArquivo, ios::binary);
+            if (!arquivo) {
+                cout << "Erro ao abrir o arquivo para carregar: " << nomeArquivo << endl;
+                return;
+            }
+
+            while (head) {
+                Bloco *temp = head;
+                head = head->proximo;
+                delete temp;
+            }
+
+            Bloco *atual = nullptr;
+            while (true) {
+                int numRegistros;
+                arquivo.read(reinterpret_cast<char*>(&numRegistros), sizeof(numRegistros));
+                if (arquivo.eof()) break;
+
+                Bloco *novoBloco = new Bloco();
+                novoBloco->numRegistros = numRegistros;
+                arquivo.read(reinterpret_cast<char*>(novoBloco->registros), sizeof(RegistroTrade) * numRegistros);
+
+                if (!head) {
+                    head = novoBloco;
+                    atual = head;
+                } else {
+                    atual->proximo = novoBloco;
+                    atual = novoBloco;
+                }
+            }
+
+            arquivo.close();
+        }
+};
+
+void lerArquivoCSV(const string &nomeArquivo, SequenceSet &sequenceSet){
+    ifstream arquivoCSV(nomeArquivo);
     if (!arquivoCSV) {
-        cout << "Erro ao abrir o arquivo CSV!" << endl;
+        cout << "Erro ao abrir o arquivo: " << nomeArquivo << endl;
         return;
     }
+    
+    string linha;
+    int contador = 0;
 
     getline(arquivoCSV, linha); // Pular o cabeçalho
 
-    while (getline(arquivoCSV, linha)) {
-        quantTrades++;
-    }
+    while(getline(arquivoCSV, linha) and contador < 500){
+        stringstream ss(linha);
+        string time_refStr, account, code, country_code, product_type, valueStr, status;
 
-    arquivoCSV.clear();
-    arquivoCSV.seekg(0, ios::beg);
-    getline(arquivoCSV, linha);
-    
-    RegistroTrade* dados = new RegistroTrade[quantTrades];
-    
-    while (arquivoCSV >> dados[i].time_ref) {
-        arquivoCSV >> lixo; // Pular a vírgula
-        arquivoCSV.getline(dados[i].account, 50, ',');
-        arquivoCSV.getline(dados[i].code, 50, ',');
-        arquivoCSV.getline(dados[i].country_code, 50, ',');
-        arquivoCSV.getline(dados[i].product_type, 50, ',');
-        arquivoCSV >> dados[i].value;
-        if (arquivoCSV.fail()) {
-            dados[i].value = 0.0; // Se falhou, atribui 0.0
-            arquivoCSV.clear(); // Limpar o estado de falha
-            arquivoCSV >> lixo; 
-        } else {
-            arquivoCSV >> lixo; 
+        if(!getline(ss, time_refStr, ',') ||
+           !getline(ss, account, ',') ||
+           !getline(ss, code, ',') ||
+           !getline(ss, country_code, ',') ||
+           !getline(ss, product_type, ',') ||
+           !getline(ss, valueStr, ',') ||
+           !getline(ss, status, ',')){
+            cout << "Erro: Linha com formato inválido: " << linha << endl;
+            continue;
         }
-        arquivoCSV.getline(dados[i].status, 10);
-        arquivoCSV.ignore();
 
-        i++;
+        if(time_refStr.empty() || account.empty() || code.empty() || country_code.empty() || product_type.empty() || valueStr.empty() || status.empty()){
+            cout << "Erro: Linha com formato inválido: " << linha << endl;
+            continue;
+        }
+
+        RegistroTrade novoRegistro;
+        try {
+            novoRegistro.time_ref = stoi(time_refStr);
+            strcpy(novoRegistro.account, account.c_str());
+            strcpy(novoRegistro.code, code.c_str());
+            strcpy(novoRegistro.country_code, country_code.c_str());
+            strcpy(novoRegistro.product_type, product_type.c_str());
+            novoRegistro.value = stod(valueStr);
+            strcpy(novoRegistro.status, status.c_str());
+        } catch (const exception &e) {
+            cout << "Erro: Linha com formato inválido: " << linha << endl;
+            continue;
+        }
+
+        sequenceSet.inserirRegistro(novoRegistro);
+        contador++;
     }
-
     arquivoCSV.close();
-
-    transformarEmBinario(dados, quantTrades);
-
-    cout << "Arquivo binario criado com sucesso!" << endl;
-
-    delete[] dados;
 }
 
-void InternationalTrade::transformarEmBinario(RegistroTrade* dados, int quantTrades) {
-    ofstream arquivoBinario("international-trade-june-2022-quarter-bin.bin", ios::binary);
-    arquivoBinario.write((char*)dados, sizeof(RegistroTrade) * quantTrades);
-    arquivoBinario.close();
-}
+void Interface(){
+    SequenceSet sequenceSet;
 
-void InternationalTrade::leituraArquivoBinario() {
-    ifstream arquivoBinario("international-trade-june-2022-quarter-bin.bin", ios::binary);
-    if (!arquivoBinario) {
-        cout << "Erro ao abrir o arquivo binário!" << endl;
-        return;
-    }
+    lerArquivoCSV("international-trade-june-2022-quarter-csv.csv", sequenceSet);
+    sequenceSet.salvarEmArquivo("blocos.dat");
 
-    arquivoBinario.seekg(0, ios::end);
-    streamsize tamanhoArquivo = arquivoBinario.tellg();
-    arquivoBinario.seekg(0, ios::beg);
+    SequenceSet loadedSequenceSet;
+    loadedSequenceSet.carregarDeArquivo("blocos.dat");
 
-    quantTrades = tamanhoArquivo / sizeof(RegistroTrade);
+   int opcao;
+	bool encontrado = true;
+	while (encontrado){
+		cout << "-------------------------------------------------MENU----------------------------------------------------"<< endl;
+		cout << "Escolha o que deseja fazer:" << endl << endl;
+		cout << "Digite [1] para fazer uma busca." << "				" << "Digite [4] para mostrar os dados." << endl;
+		cout << "Digite [2] para fazer uma insercao." << "				" << "Digite [0] para encerrar o programa." << endl;
+        cout << "Digite [3] para fazer uma remocao." << endl;
+		cin >> opcao;
+		cout << endl;
 
-    RegistroTrade* dados = new RegistroTrade[quantTrades];
-    
-    arquivoBinario.read((char*)dados, tamanhoArquivo);
-    
-    arquivoBinario.close();
+		switch (opcao) {
+		case 1:
+			cout << "em desenvolvimento..." << endl;
+			break;
 
-    delete[] dados;
+		case 2:
+			cout << "em desenvolvimento..." << endl;
+			break;
+
+		case 3:
+			cout << "em desenvolvimento..." << endl;
+			break;
+
+        case 4:
+            loadedSequenceSet.exibirTodos();
+            cout << "Total de blocos carregados: " << loadedSequenceSet.contarBlocos() << endl;
+            break;
+
+		case 0:
+			encontrado = false;
+			cout << "Encerrando programa..." << endl;
+			break;
+
+		default:
+            cout << "Opcao invalida!" << endl;
+		break;
+		}
+	}
 }
 
 int main() {
-    int opcao;
-    InternationalTrade trade;
-
-    cout << "Escolha uma opcao: " << endl
-    << "1 - Converter a leitura do arquivo CSV para o formato binario." << endl
-    << "2 - Acessar arquivo binario." << endl;
-    cin >> opcao;
-    switch (opcao) {
-        case 1:
-            trade.leituraArquivoCSV();
-            break;
-        case 2:
-            trade.leituraArquivoBinario();
-            break;
-        default:
-            cout << "Opção inválida!" << endl;
-            break;
-    }
-
+    Interface();
     return 0;
 }
